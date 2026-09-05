2a7945923a09c937ccff0c5ca1d323c366718dba
postcoglu <opolot.johnp256@gmail.com>
2026-09-05 13:24:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+ a.addEventListener('click',function(e){
+   e.preventDefault();
+   const t=document.querySelector(this.getAttribute('href'));
+   if(t) t.scrollIntoView({behavior:'smooth'});
+ });
+});
